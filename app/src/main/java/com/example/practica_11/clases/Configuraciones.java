0bb89669705d7ff433c60dd50b57aad27974300c
MarAlfaro <alfaromargarita556@gmail.com>
2023-04-18 18:49:21 -0600

    Initial commit

---
+++ app/src/main/java/com/example/practica_11/clases/Configuraciones.java
@@ -0,0 +1,8 @@
+package com.example.practica_11.clases;
+
+public class Configuraciones {
+    public String urlWebServices;
+    public Configuraciones(){
+        this.urlWebServices = "http://192.168.1.48/ws_app/webservices02.php";
+    }
+}
